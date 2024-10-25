f21648d091f74694deb2e58bee94f7a0f32261ac
Felix@Work <felix.neumann@haus-der-edv.de>
2024-10-25 11:36:19 +0200

    Projektdateien hinzufügen.

---
+++ Key_IV_ Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class KeyGenerator
+{
+    public static void Main()
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.KeySize = 128;
+            aes.GenerateKey();
+            aes.GenerateIV();
+
+            string key = Convert.ToBase64String(aes.Key);
+            string iv = Convert.ToBase64String(aes.IV);
+
+            Console.WriteLine("Key: " + key);
+            Console.WriteLine("IV: " + iv);
+        }
+    }
+}
